a9e3776f814f7bd6bfc7af0db31c8f5cb8ce428a
palaktd <palaktiwari2805@icloud.com>
2024-12-19 18:53:05 +0530

    Update enable_swap_space.sh

--- enable_swap_space.sh
+++ enable_swap_space.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-read -p "Do you need Swap space for your work?(True/False): " swap
+read -p "Do you need Swap space for your work?(Yes/No): " swap
 
-if [[ $swap =~ [tT]rue ]]; then 
+if [[ $swap =~ [Yy]es ]]; then 
         read -p "Enter the name of swap space/file you want to create: " swap_filename
         read -p "Enter the size of the swapfile (make sure to add unit to the value such as G,M,B): " size_of_swapfile
 
@@ -21,24 +21,27 @@ if [[ $swap =~ [tT]rue ]]; then
 
         ### Checking the total memory of swap ###
         swap_mem="$(free -t|awk 'NR==3 {print $2}')"
-
+        echo "Your current swap size is $swap_mem"
         if [[ $swap_mem == 0 ]]; then 
                 ### Allocating desired swap space to swap file ###
                 sudo fallocate -l $size_of_swapfile $swap_filename
 
                 ### Marking file as swap ###
-                sudo mkswap $swap_filename
+                sudo mkswap $swap_filename > /dev/null
 
                 ### Enabling the swap file ###
                 sudo swapon $swap_filename
 
                 ### Making Swap file permanent ###
                 echo "$swap_filename none swap sw 0 0" | sudo tee -a /etc/fstab > /dev/null
+                echo "Swap space enabling steps are complete"
 
                 ### Adjusting the Swappiness Property ###
                 swappiness="$(cat /proc/sys/vm/swappiness)" 
                         if [[ $swappiness -ge 40 && $swappiness -le 60 ]]; then 
-                                echo "no change to swappiness parameter"
+                                echo "Checking the swappiness parameter...."
+                                echo "Swappiness Paramater in range"
+                                echo "No change to swappiness parameter"
                                 else 
                                 swappiness=60
                                 echo $swappiness | sudo tee -a /proc/sys/vm/swappiness
@@ -51,6 +54,7 @@ if [[ $swap =~ [tT]rue ]]; then
                         echo "********************************"
                         echo "Swap is enabled and ready to use"
                         echo "********************************"
+			echo "Your latest swap size is $(free -t|awk 'NR==3 {print $2}')B"
                 else
                         echo "************************************************************************"
                         echo "Swapping procedure failed. Re-run the script or check the logs for error"
